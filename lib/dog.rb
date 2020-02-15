eadeac1f4d03719e6d3e38a8f02b3d1f22dd3743
T S <taneymail@gmail.com>
2020-02-15 21:00:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+class Dog
+
+  @@all=[]
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name= name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    dogs_moniker= name
+    self.all.collect do |dogs_moniker| dogs_moniker.name
+    puts dogs_moniker.name.to_s
+  end
+end
+    # binding.pry
+  def save
+    # binding.pry
+    @@all<< self
+    self
+  end
+
+end
